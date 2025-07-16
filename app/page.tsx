2c0790f41e43ed8d599d79fd6a1e8b27c9827ddc
Kiran S Raddi <kkraddi111@gmail.com>
2025-07-16 23:56:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -205,8 +205,8 @@ export default function Portfolio() {
               <Image
                 src="/Kiran-avatar.jpg"
                 alt="Kiran Raddi Avatar"
-                width={160}
-                height={160}
+                width={200}
+                height={200}
                 className="w-32 h-32 md:w-40 md:h-40 rounded-full mx-auto object-cover border-4 border-border shadow-lg"
                 priority
               />
